70a55384634a5e7fc28cfd98120610aac4f6a5b3
Luis Asanza <luisasanza@hotmail.com>
2025-08-19 00:50:23 -0300

    Add CitiesService with IoC

---
+++ DIExample/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+
+namespace DIExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICitiesService _citiesService;
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+
+        public HomeController(ICitiesService citiesService, IServiceScopeFactory serviceScopeFactory)
+        {
+            _citiesService = citiesService;
+            _serviceScopeFactory = serviceScopeFactory;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            var cities = _citiesService.GetCities();
+            List<string> citiesInChildScope = new List<string>();
+
+            using (IServiceScope scope = _serviceScopeFactory.CreateScope()) {
+                var citiesService = scope.ServiceProvider.GetService<ICitiesService>();
+                citiesInChildScope = citiesService == null ? new List<string>() :  citiesService.GetCities();
+            }
+
+
+            return View(citiesInChildScope);
+        }
+    }
+}
--- DIExample/Program.cs
+++ DIExample/Program.cs
@@ -1,5 +1,11 @@
+using ServiceContracts;
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+//builder.Services.Add(new ServiceDescriptor(typeof(ICitiesService), typeof(CitiesService), ServiceLifetime.Scoped));
+//Short hand method version:
+builder.Services.AddScoped<ICitiesService, CitiesService>();
 
 var app = builder.Build();
 
